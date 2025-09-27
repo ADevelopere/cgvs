7b1fbf1e3f48ed659bfb6b808ffbccd510e6b326
Ahmed Hosny <ahhosnyas@gmail.com>
2025-09-27 07:20:33 +0300

    remove ktor

--- graphql/graphql-ts/storage/mutation/generateUploadSignedUrl.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const GENERATE_UPLOAD_SIGNED_URL = gql`
-mutation generateUploadSignedUrl($input: GenerateUploadSignedUrlInput!){
-    generateUploadSignedUrl(input: $input)
-}
-`;
