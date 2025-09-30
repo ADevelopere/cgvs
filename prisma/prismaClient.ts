de67c885e29672f22df5559fc6844a39520c71da
Ahmed Hosny <ahhosnyas@gmail.com>
2025-09-30 19:37:55 +0300

    Remove Prisma and related dependencies

--- prisma/prismaClient.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "./generated/client";
-import { drizzle } from "drizzle-orm/prisma/pg";
-// import { drizzle } from 'drizzle-orm/prisma/mysql';
-
-const prismaClient = new PrismaClient().$extends(drizzle());
-
-export default prismaClient;
