28d272f602e1eafa02b2be2cbf05603c4b97dc72
ahmedhosnypro <ahhosnyas@gmail.com>
2025-10-05 01:02:11 +0300

    feat: implement admin layout and routing structure with protected routes and storage management

---
+++ app/(root)/(auth)/admin/categories/page.tsx
@@ -0,0 +1,8 @@
+import TemplateCategoryManagement from "@/client/views/templateCategory/management/CategoryManagement";
+import React from "react";
+
+export default function Page() {
+    return (
+        <TemplateCategoryManagement />
+    );
+}
---
+++ app/(root)/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import ProtectedRoute from "@/client/components/ProtectedRoute";
+import StorageProvider from "@/client/views/storage/StorageProvider";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <ProtectedRoute>
+            <StorageProvider>{children}</StorageProvider>
+        </ProtectedRoute>
+    );
+}
