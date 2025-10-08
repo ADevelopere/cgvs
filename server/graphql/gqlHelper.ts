6f318889da8e953d95d95651fe71df3ae0f00ed4
Ahmed Hosny <ahhosnyas@gmail.com>
2025-10-08 14:39:59 +0300

    feat: Implement recipient group types and omit fields for input types

--- server/graphql/gqlHelper.ts
+++ server/graphql/gqlHelper.ts
@@ -2,3 +2,10 @@
 export type OmitIdRelationFields<T> = {
     [K in keyof T as K extends `${string}Id` ? never : K]: T[K];
 };
+
+// omit "id" | "createdAt" | "updatedAt"
+export type OmitEntityFields<T> = {
+    [K in keyof T as K extends "id" | "createdAt" | "updatedAt"
+        ? never
+        : K]: T[K];
+};
