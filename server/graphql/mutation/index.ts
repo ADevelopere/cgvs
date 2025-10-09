5b16e5c39e423e36ca6adb6aa71eff612fde6129
ahmedhosnypro <ahhosnyas@gmail.com>
2025-10-09 15:52:57 +0300

    feat: Refactor GraphQL schema imports and create separate index files for queries and mutations

---
+++ server/graphql/mutation/index.ts
@@ -0,0 +1,7 @@
+import "./auth.mutation";
+import "./recipientGroup.mutation";
+import "./storage.mutation";
+import "./student.mutation";
+import "./template.mutation";
+import "./templateCategory.mutation";
+import "./templateVariable.mutation";
