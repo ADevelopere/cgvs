9ed484f694e195cce0ae58cac15f9669cf6227a9
Ahmed Hosny <ahhosnyas@gmail.com>
2025-10-11 00:38:37 +0300

    refactor: improve code formatting and structure in layout and login components

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -2,25 +2,27 @@
 
 import LoginForm from "@/client/views/auth/LoginForm";
 import { Container } from "@mui/material";
+import { Suspense } from "react";
 import type React from "react";
 
 const Login: React.FC = () => {
-    return (
-        <Container
-            maxWidth="sm"
-            sx={{
-                py: 8,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                padding: 3,
-                height: "100vh",
-            }}
-        >
-            <LoginForm />
-        </Container>
-    );
-    
+ return (
+  <Container
+   maxWidth="sm"
+   sx={{
+    py: 8,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+    height: "100vh",
+   }}
+  >
+   <Suspense fallback={<div>Loading...</div>}>
+    <LoginForm />
+   </Suspense>
+  </Container>
+ );
 };
 
 export default Login;
