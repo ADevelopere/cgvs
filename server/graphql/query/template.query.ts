1c9226cda0b21a61d6ec45262a14ecc3f4e85a96
Ahmed Hosny <ahhosnyas@gmail.com>
2025-10-12 09:01:40 +0300

    refactor: update template management and GraphQL queries for improved structure and functionality
    - Adjusted indentation settings in .editorconfig for consistency.
    - Refactored TemplateCategoryManagementContext and TemplateManagementContext to enhance readability and maintainability.
    - Updated GraphQL queries and mutations to streamline data fetching and improve type safety.
    - Introduced new queries for suspended templates and templates by category ID, enhancing the flexibility of template management.
    - Improved error handling and state management in template-related operations, ensuring robust functionality across the application.

--- server/graphql/query/template.query.ts
+++ server/graphql/query/template.query.ts
@@ -1,43 +1,57 @@
 import { gqlSchemaBuilder } from "../gqlSchemaBuilder";
 import { PaginationArgs } from "@/server/types";
 import {
-    PaginatedTemplatesResponsePothosObject,
-    TemplatePothosObject,
-    TemplatesConfigsPothosObject,
-    PaginationArgsObject
+  PaginatedTemplatesResponsePothosObject,
+  TemplatePothosObject,
+  TemplatesConfigsPothosObject,
+  PaginationArgsObject,
 } from "@/server/graphql/pothos";
 import { TemplateRepository } from "@/server/db/repo";
 
 gqlSchemaBuilder.queryFields((t) => ({
-    template: t.field({
-        type: TemplatePothosObject,
-        nullable: true,
-        args: {
-            id: t.arg.int({ required: true }),
-        },
-        resolve: async (_query, args) => TemplateRepository.findById(args.id),
-    }),
+  template: t.field({
+    type: TemplatePothosObject,
+    nullable: true,
+    args: {
+      id: t.arg.int({ required: true }),
+    },
+    resolve: async (_query, args) => await TemplateRepository.findById(args.id),
+  }),
 
-    templates: t.field({
-        type: PaginatedTemplatesResponsePothosObject,
-        args: {
-            pagination: t.arg({
-                type: PaginationArgsObject,
-            }),
-        },
-        resolve: async (_, args) =>
-            TemplateRepository.findAllPaginated(
-                new PaginationArgs({ ...args.pagination }),
-            ),
-    }),
+  templates: t.field({
+    type: PaginatedTemplatesResponsePothosObject,
+    args: {
+      pagination: t.arg({
+        type: PaginationArgsObject,
+      }),
+    },
+    resolve: async (_, args) =>
+      await TemplateRepository.findAllPaginated(
+        new PaginationArgs({ ...args.pagination }),
+      ),
+  }),
 
-    templatesConfigs: t.field({
-        type: TemplatesConfigsPothosObject,
-        resolve: async () => {
-            const conf = await TemplateRepository.allConfigs();
-            return {
-                configs: conf,
-            };
-        },
-    }),
+  templatesByCategoryId: t.field({
+    type: [TemplatePothosObject],
+    args: {
+      categoryId: t.arg.int({ required: true }),
+    },
+    resolve: async (_, args) =>
+      await TemplateRepository.findByCategoryId(args.categoryId),
+  }),
+
+  suspendedTemplates: t.field({
+    type: [TemplatePothosObject],
+    resolve: async () => await TemplateRepository.suspendedTemplates(),
+  }),
+
+  templatesConfigs: t.field({
+    type: TemplatesConfigsPothosObject,
+    resolve: async () => {
+      const conf = await TemplateRepository.allConfigs();
+      return {
+        configs: conf,
+      };
+    },
+  }),
 }));
