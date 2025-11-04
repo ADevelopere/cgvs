e59c5f59a5c3d04fbe8a19a639cb01e0bcb1724c
ahmedhosnypro <ahhosnyas@gmail.com>
2025-11-04 13:24:00 +0000

    tem

--- client/views/template/manage/editor/form/config/useTemplateConfigMutation.ts
+++ client/views/template/manage/editor/form/config/useTemplateConfigMutation.ts
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { useMutation } from "@apollo/client/react";
-import * as GQL from "@/client/graphql/generated/gql/graphql";
 import * as Documents from "../../glqDocuments/config.documents";
 
 export const useTemplateConfigMutation = () => {
@@ -28,79 +27,17 @@ export const useTemplateConfigMutation = () => {
             templateConfigByTemplateId: newTemplateConfig,
           },
         });
-        // const newConfig = data.createTemplateConfig;
-        // const templateId = newConfig.templateId;
-        // if (!templateId) return;
-
-        // cache.updateQuery<GQL.TemplateConfigByTemplateIdQuery>(
-        //   {
-        //     query: Documents.templateConfigByTemplateIdQueryDocument,
-        //     variables: { templateId },
-        //   },
-        //   existing => {
-        //     if (!existing?.templateConfigByTemplateId) return existing;
-        //     return {
-        //       templateConfigByTemplateId: newConfig,
-        //     };
-        //   }
-        // );
       },
     }
   );
 
-  const [updateTemplateConfigMutation] = useMutation(
-    Documents.updateTemplateConfigMutationDocument,
-    {
-      // This config object is re-created if 'existingElement' changes,
-      // which is exactly what we want.
-      optimisticResponse: variables => {
-        const { input } = variables;
-
-        return {
-          // Key matches your mutation name
-          updateTemplateConfig: {
-            __typename: "TemplateConfig" as const,
-            ...input,
-            createdAt: null,
-            updatedAt: null,
-            templateId: null,
-          },
-        };
-      },
-      update(cache, { data }) {
-        if (!data?.updateTemplateConfig) return;
-        const updated = data.updateTemplateConfig;
-        const templateId = updated.templateId;
-        if (!templateId) return;
-
-        cache.updateQuery<GQL.TemplateConfigByTemplateIdQuery>(
-          {
-            query: Documents.templateConfigByTemplateIdQueryDocument,
-            variables: { templateId },
-          },
-          existing => {
-            if (!existing?.templateConfigByTemplateId) return existing;
-            return {
-              templateConfigByTemplateId: updated,
-            };
-          }
-        );
-      },
-    }
-  );
-
-  // Return the same tuple as useMutation
   return React.useMemo(
     () => ({
       /**
-       * Mutation to create a new template config
+       * Mutation to create a new template config, and write it to templateConfigByTemplateIdQuery
        */
       createTemplateConfigMutation,
-      /**
-       * Mutation to update an existing template config
-       */
-      updateTemplateConfigMutation,
     }),
-    [createTemplateConfigMutation, updateTemplateConfigMutation]
+    [createTemplateConfigMutation]
   );
 };
