e10981c6c77fdcc18b13a8160205f6a1ad794f54
ahmedhosnypro <ahhosnyas@gmail.com>
2025-11-05 05:07:37 +0000

    feat: Add font embedding utility to load Google Fonts dynamically in CertificateRenderer

--- client/views/template/manage/editor/renderer/CertificateRenderer.tsx
+++ client/views/template/manage/editor/renderer/CertificateRenderer.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import * as GQL from "@/client/graphql/generated/gql/graphql";
 import { PureTextElement } from "./PureTextElement";
 import { resolveTextContent } from "./textResolvers";
-import { FontFamily, getFontByFamily } from "@/lib/font/google";
-import { getFontFamilyString } from "../nodeRenderer/utils";
-import WebFont from "webfontloader";
+import { FontFamily } from "@/lib/font/google";
+import { embedGoogleFonts } from "@/lib/font/fontUtils";
 
 export interface CertificateRendererProps {
   elements: GQL.CertificateElementUnion[];
@@ -28,6 +27,7 @@ export const CertificateRenderer: React.FC<CertificateRendererProps> = ({
   onReady,
 }) => {
   const [fontsLoaded, setFontsLoaded] = React.useState(false);
+  const [fontStyles, setFontStyles] = React.useState<string>("");
   const [textElements, setTextElements] = React.useState<TextElementData[]>(
     []
   );
@@ -39,39 +39,25 @@ export const CertificateRenderer: React.FC<CertificateRendererProps> = ({
       .sort((a, b) => a.base.renderOrder - b.base.renderOrder);
 
     // Collect unique Google fonts to load
-    const fontsToLoad: string[] = [];
+    const uniqueFonts = new Set<FontFamily>();
     const fontFamilyMap = new Map<number, string>();
-    let needsRoboto = false;
 
     textEls.forEach(el => {
       if (el.textProps.fontRef.__typename === "FontReferenceGoogle") {
-        const identifier = el.textProps.fontRef.identifier;
+        const identifier = el.textProps.fontRef.identifier as FontFamily;
         if (identifier) {
-          const font = getFontByFamily(identifier as FontFamily);
-          if (font) {
-            const fontString = getFontFamilyString(font);
-            if (fontString && !fontsToLoad.includes(fontString)) {
-              fontsToLoad.push(fontString);
-            }
-            fontFamilyMap.set(el.base.id, identifier);
-          }
+          uniqueFonts.add(identifier);
+          fontFamilyMap.set(el.base.id, identifier);
         } else {
-          // Fallback if identifier is missing
           fontFamilyMap.set(el.base.id, "Roboto");
-          needsRoboto = true;
+          uniqueFonts.add(FontFamily.ROBOTO);
         }
       } else {
         // TODO: Add support for self-hosted fonts
-        // For now, default self-hosted fonts to use Roboto from Google
         fontFamilyMap.set(el.base.id, "Roboto");
-        needsRoboto = true;
+        uniqueFonts.add(FontFamily.ROBOTO);
       }
     });
-
-    // Add Roboto if needed as fallback
-    if (needsRoboto && !fontsToLoad.includes("Roboto")) {
-      fontsToLoad.push("Roboto");
-    }
 
     // Resolve text content for each element
     const resolvedElements: TextElementData[] = textEls.map(el => ({
@@ -81,31 +67,23 @@ export const CertificateRenderer: React.FC<CertificateRendererProps> = ({
         config.language,
         "Text"
       ),
-      fontFamily: fontFamilyMap.get(el.base.id) || "Cairo",
+      fontFamily: fontFamilyMap.get(el.base.id) || "Roboto",
     }));
 
     setTextElements(resolvedElements);
 
-    // Load fonts
-    if (fontsToLoad.length > 0) {
-      WebFont.load({
-        google: {
-          families: fontsToLoad,
-        },
-        active: () => {
-          setFontsLoaded(true);
-          onReady?.();
-        },
-        inactive: () => {
-          // Even if fonts fail, proceed
-          setFontsLoaded(true);
-          onReady?.();
-        },
+    // Embed fonts
+    embedGoogleFonts(Array.from(uniqueFonts))
+      .then(styles => {
+        setFontStyles(styles);
+        setFontsLoaded(true);
+        onReady?.();
+      })
+      .catch(() => {
+        // Even if fonts fail, proceed
+        setFontsLoaded(true);
+        onReady?.();
       });
-    } else {
-      setFontsLoaded(true);
-      onReady?.();
-    }
   }, [elements, config.language, onReady]);
 
   if (!fontsLoaded) {
@@ -122,6 +100,7 @@ export const CertificateRenderer: React.FC<CertificateRendererProps> = ({
         overflow: "hidden", // Container clips children
       }}
     >
+      <div dangerouslySetInnerHTML={{ __html: fontStyles }} />
       {textElements.map(({ element, textContent, fontFamily }) => (
         <PureTextElement
           key={element.base.id}
